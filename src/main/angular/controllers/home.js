5caeb5ab38e47dfcd8a4b59af283f970237b2d42
Olivier ORAND <olivier.orand@orange.com>
2015-12-07 18:22:56 +0100

    fix(angular): display error message
    Instead of adding a timeout, it handles error in angular response.
    
    close #2

--- src/main/angular/controllers/home.js
+++ src/main/angular/controllers/home.js
@@ -1,12 +1,29 @@
 angular.module('sandbox').controller('home', function ($scope, $http) {
     $scope.showprogressbar = true;
     $scope.connectionPhrase = "";
+    $scope.messageTitle = 'Please wait...';
+    $scope.messageBody = 'We\'re setting up the Cloud Foundry platform with a private space.';
+    $scope.showConnectionDetails = false;
+
     $http.get('v1/sandboxes/me').success(function (data) {
-        $scope.showprogressbar = false;
-        $scope.connectionPhrase = "cf login --skip-ssl-validation -a " + data.apiUrl + " -u " + data.spaceName + " -o " + data.orgName + " -s " + data.spaceName;
-        $scope.sandbox = data;
-    });
+            $scope.showprogressbar = false;
+            $scope.connectionPhrase = "cf login --skip-ssl-validation -a " + data.apiUrl + " -u " + data.spaceName + " -o " + data.orgName + " -s " + data.spaceName;
+            $scope.sandbox = data;
+            $scope.messageTitle = "You're all set.";
+            $scope.messageBody = "You're all set to discover the Cloud Foundry platform: a private space is now allocated to you for playing and trying things out.";
+            $scope.showConnectionDetails = true;
+        })
+        .error(function (error) {
+            $scope.showprogressbar = false;
+            $scope.messageTitle = "Sorry, something goes wrong.";
+            $scope.messageBody = "Error: " + error;
+            Materialize.toast("Try again or contact your administrator !", 5000, 'rounded');
+            jQuery('#toast-container').click(function(){
+                jQuery(this).remove();
+            });
+        });
     $scope.showMessageCopied = function () {
         Materialize.toast('Copied !', 2000);
     };
+
 });
--- src/main/angular/directives/prism.js
+++ src/main/angular/directives/prism.js
@@ -1,14 +1,18 @@
 angular.module('sandbox')
     .directive('ngPrism', ['$interpolate', function ($interpolate) {
-        "use strict";
+        //"use strict";
         return {
             restrict: 'E',
             template: '<pre><code ng-transclude></code></pre>',
             replace: true,
             transclude: true,
-            link: function (scope, elm) {
-                var tmp = $interpolate(elm.find('code').text())(scope);
-                elm.find('code').html(Prism.highlightElement(tmp).value);
+            link: function (scope, element) {
+                element.ready(function () {
+                    var tmp = $interpolate(element.find('code').text())(scope);
+                    if (tmp != undefined && tmp != ""){
+                        element.find('code').html(Prism.highlightElement(tmp).value);
+                    }
+                });
             }
         };
     }]);
