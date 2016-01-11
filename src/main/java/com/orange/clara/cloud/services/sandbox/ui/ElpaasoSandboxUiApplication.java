e90266e228fabe697c8a2fb48120554bec816dcf
Olivier ORAND <olivier.orand@orange.com>
2016-01-11 14:46:22 +0100

    update license header and use elpaaso-starter-parent

--- src/main/java/com/orange/clara/cloud/services/sandbox/ui/ElpaasoSandboxUiApplication.java
+++ src/main/java/com/orange/clara/cloud/services/sandbox/ui/ElpaasoSandboxUiApplication.java
@@ -1,6 +1,5 @@
-
-/*
- * Copyright (C) 2015 Orange
+/**
+ * Copyright (C) 2015-2016 Orange
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,9 +9,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
-
 package com.orange.clara.cloud.services.sandbox.ui;
 
 import com.orange.clara.cloud.services.sandbox.ui.config.XTrustProvider;
